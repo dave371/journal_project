ef9d90935741e6acd90ec75de0b66ed55a516f5a
dave731 <davepacheco456@gmail.com>
2022-03-25 14:02:53 -0400

    constant variables

---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,8 @@
+export const FETCH_ALL = "FETCH_ALL";
+export const FETCH = "FETCH";
+export const CREATE = "CREATE";
+export const UPDATE = "UPDATE";
+export const DELETE = "DELETE";
+export const SET = "SET";
+export const GET = "GET";
+export const RESET = "RESET";
